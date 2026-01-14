67c6fa323ee4c456ed642410f7d185f3c5fc2599
Amal P Sreenivasan <amal.p@emvigotech.com>
2026-01-14 15:51:46 +0530

    port changes added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 const connectDB = async () => {
   try {
